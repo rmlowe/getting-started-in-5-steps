9ac4248604644bd93dec05352573a76aba6b12da
Robert Lowe <robertmlowe@rmlowe.com>
2019-01-11 15:28:11 +0800

    Step 10 : Spring Boot Developer Tools

--- springboot-in-10-steps/src/main/java/com/in28minutes/springboot/basics/springbootin10steps/BooksController.java
+++ springboot-in-10-steps/src/main/java/com/in28minutes/springboot/basics/springbootin10steps/BooksController.java
@@ -11,6 +11,6 @@ public class BooksController {
 	@GetMapping("/books")
 	public List<Book> getAllBooks() {
 		return Arrays.asList(
-				new Book(1l, "Mastering Spring 5.0", "Ranga Karanam"));
+				new Book(1l, "Mastering Spring 5.2", "Ranga Karanam"));
 	}
 }
